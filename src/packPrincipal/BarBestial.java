c1641f86d31668a004d82e31aa9abd8161a83df0
eereno21 <46158814+eereno21@users.noreply.github.com>
2019-01-10 19:26:10 +0100

    Update BarBestial.java

--- src/packPrincipal/BarBestial.java
+++ src/packPrincipal/BarBestial.java
@@ -71,4 +71,37 @@ public class BarBestial {
     	info.put("usadas", usadas);
     	return info;
     }
+
+	//ENERITZ
+    public boolean registro (JSONObject obj1) {
+    	if (GestorJugador.getGestorJugador().comprobarId(obj1.getString("idJ"))){
+    		GestorJugador.getGestorJugador().registrar(obj1);
+    		return true;
+    	}else {
+    		return false;
+    	}
+    }
+    
+    public boolean inicioSesion (String idJ, String pass) {
+    	if (GestorJugador.getGestorJugador().comprobarPass(idJ, pass)) {
+    		GestorJugador.getGestorJugador().inicioSesion(idJ);
+    		return true;
+    	}else {
+    		return false;
+    	}
+    }
+    
+    public boolean cambiar (JSONObject obj2) {
+    	String idJ = obj2.getString("idJ");
+    	if (GestorJugador.getGestorJugador().comprobarPass(idJ, obj2.getString("password"))) {
+    		GestorJugador.getGestorJugador().cambiar(idJ, obj2.getString("newPass"));
+    		return true;
+    	}else{
+    		return false;
+    	}
+    }
+    
+    public boolean recuperar (JSONObject obj3) { 
+    	return GestorJugador.getGestorJugador().comprobarDatos(obj3);
+    }
 }
